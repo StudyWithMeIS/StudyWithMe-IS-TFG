84ea10a6b638a390ec9c4833d7f980548ae85ab9
IslaP-DEV <isla.peinado@campusfp.es>
2024-03-13 13:28:17 +0100

    base de datos terminada

--- src/main/java/swm/studywithmeistfg/repositories/AlumnoRepository.java
+++
@@ -1,8 +0,0 @@
-package swm.studywithmeistfg.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import swm.studywithmeistfg.classes.Alumno;
-
-public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
-    // Puedes agregar métodos personalizados si es necesario
-}
