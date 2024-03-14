d4bf757f2af5ee161e139a1d48b72c0193937460
IslaP-DEV <isla.peinado@campusfp.es>
2024-03-14 17:33:08 +0100

    actualizaciones minimas en las entidades y el controlaor

--- src/main/java/com/example/swm/security/RolManagement.java
+++ src/main/java/com/example/swm/security/RolManagement.java
@@ -1,49 +1,52 @@
 package com.example.swm.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-//DEMOMENTO PEGA OSTIA PERO ESO SE ARREGLA CUANDO SE CONECTE A LA BASE DE DATOS
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
-public class RolManagement extends WebSecurityConfigurerAdapter  {
+public class RolManagement {
     @Autowired
     private DataSource dataSource;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT email_admin AS username, password_admin AS password, true FROM administradores WHERE email_admin = ?")
-                .authoritiesByUsernameQuery("SELECT email_admin AS username, 'ROLE_ADMIN' AS authority FROM administradores WHERE email_admin = ?")
-                .passwordEncoder(passwordEncoder());
-
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT email_profesor AS username, password_profesor AS password, true FROM profesores WHERE email_profesor = ?")
-                .authoritiesByUsernameQuery("SELECT email_profesor AS username, 'ROLE_PROFESOR' AS authority FROM profesores WHERE email_profesor = ?")
-                .passwordEncoder(passwordEncoder());
-
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT email_alumno AS username, password_alumno AS password, true FROM alumnos WHERE email_alumno = ?")
-                .authoritiesByUsernameQuery("SELECT email_alumno AS username, 'ROLE_ALUMNO' AS authority FROM alumnos WHERE email_alumno = ?")
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/profesor/**").hasRole("PROFESOR")
-                .antMatchers("/alumno/**").hasRole("ALUMNO")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery("SELECT nif_admin AS username, password_admin AS password, true AS enabled FROM administradores WHERE nif_admin=?");
+        userDetailsManager.setAuthoritiesByUsernameQuery("SELECT nif_admin AS username, 'ROLE_ADMIN' AS authority FROM administradores WHERE nif_admin=? UNION " +
+                "SELECT nif_profesor AS username, 'ROLE_PROFESOR' AS authority FROM profesores WHERE nif_profesor=? UNION " +
+                "SELECT nif_alumno AS username, 'ROLE_ALUMNO' AS authority FROM alumnos WHERE nif_alumno=?");
+        return userDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**", "/img/**", "/scss/**", "/vendor/**").permitAll()
+                        .requestMatchers("/", "/login", "/signup").permitAll()
+                        .requestMatchers("/profesor/**").hasAuthority("ROLE_PROFESOR")
+                        .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/alumno/**").hasAuthority("ROLE_ALUMNO")
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin.loginPage("/login").permitAll())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/").permitAll())
+                .exceptionHandling((exception) -> exception.accessDeniedPage("/denegado"));
+
+        return http.build();
     }
 }
