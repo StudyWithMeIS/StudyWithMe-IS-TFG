4ec80a14e1e425e12e3984d81478f34043b0cd6e
IslaP-DEV <isla.peinado@campusfp.es>
2024-04-24 19:23:32 +0200

    Features backend y frontend. Backed contempla alumnoclase y asignatura. Fontend AÑADIDA asignatura, actualizadas el resto de clases

--- src/main/java/com/example/swm/controllers/AdministradoresController.java
+++ src/main/java/com/example/swm/controllers/AdministradoresController.java
@@ -1,29 +1,45 @@
 package com.example.swm.controllers;
 
 import com.example.swm.entity.Alumnos;
+import com.example.swm.entity.Asignaturas;
+import com.example.swm.entity.Clases;
 import com.example.swm.repository.AdministradoresRepository;
 import com.example.swm.repository.AlumnosRepository;
+import com.example.swm.repository.AsignaturasRepository;
+import com.example.swm.repository.ClasesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/administradores")
 public class AdministradoresController {
+
     @Autowired
     private AdministradoresRepository administradoresRepository;
 
     @Autowired
     private AlumnosRepository alumnosRepository;
 
+    @Autowired
+    private ClasesRepository clasesRepository;
+
+    @Autowired
+    private AsignaturasRepository asignaturasRepository;
+
+
+
+    //---------------ALUMNO----------------------
     // PRIMERO CARGA LA VISTA PARA CREAR ALUMNO.
     @GetMapping("/vistaCrearAlumnoAdmin")
     public ModelAndView mostrarPaginaAddAlumnos() {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("pages/administradores/alumnos/addAlumnosAdmin.html");
+        mv.setViewName("pages/administrador/student/addStudientAdmin");
         return mv;
     }
 
@@ -50,4 +66,81 @@ public class AdministradoresController {
         return modelAndView;
     }
 
+    // TERCERO CARGA LA VISTA PARA LISTAR ALUMNOS.
+    @GetMapping("/listarAlumnosAdmin")
+    public ModelAndView listarAlumnos() {
+        ModelAndView mv = new ModelAndView();
+        List<Alumnos> alumnos = alumnosRepository.findAll();
+        mv.addObject("alumnos", alumnos);
+        mv.setViewName("pages/administrador/student/readStudientAdmin");
+        return mv;
+    }
+
+
+    //-----------------CURSO----------------------
+    // PRIMERO CARGA LA VISTA PARA CREAR CLASE.
+    @GetMapping("/vistaCrearClaseAdmin")
+    public ModelAndView mostrarPaginaAddClase() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("pages/administrador/grade/addGradeAdmin");
+        return mv;
+    }
+
+    // SEGUNDO CARGA EL METODO QUE LLAMA DESDE EL FORMULARIO DE LA VISTA DE CREAR CLASE.
+    @RequestMapping("/guardarClase")
+    public ModelAndView guardarClase(@ModelAttribute Clases clase) {
+        ModelAndView modelAndView = new ModelAndView();
+        Clases clases = new Clases();
+        System.out.println(clase.getNombre_clase());
+        clases.setNombre_clase(clase.getNombre_clase());
+        modelAndView.addObject("mensaje", "Clase creada correctamente");
+        modelAndView.setViewName("redirect:/addClaseAdmin");
+        return modelAndView;
+    }
+
+    // TERCERO CARGA LA VISTA PARA LISTAR CLASES.
+    @GetMapping("/listarClasesAdmin")
+    public ModelAndView listarClases() {
+        ModelAndView mv = new ModelAndView();
+        List<Clases> clases = clasesRepository.findAll();
+        mv.addObject("clases", clases);
+        mv.setViewName("pages/administrador/grade/readGradeAdmin");
+        return mv;
+    }
+
+    //-----------------ASIGNATURA----------------------
+    // PRIMERO CARGA LA VISTA PARA CREAR ASIGNATURA.
+    @GetMapping("/vistaCrearAsignaturaAdmin")
+    public ModelAndView mostrarPaginaAddAsignatura() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("pages/administrador/subject/addSubjectAdmin");
+        return mv;
+    }
+
+    // SEGUNDO CARGA EL METODO QUE LLAMA DESDE EL FORMULARIO DE LA VISTA DE CREAR ASIGNATURA.
+    @RequestMapping("/guardarAsignatura")
+    public ModelAndView guardarAsignatura(@ModelAttribute Asignaturas asignatura) {
+        ModelAndView modelAndView = new ModelAndView();
+        Asignaturas asignaturas = new Asignaturas();
+        System.out.println(asignatura.getNombre_asignatura());
+        System.out.println(asignatura.getProfesor());
+        asignaturas.setNombre_asignatura(asignatura.getNombre_asignatura());
+        asignaturas.setProfesor(asignatura.getProfesor());
+        modelAndView.addObject("mensaje", "Asignatura creada correctamente");
+        modelAndView.setViewName("redirect:/addAsignaturaAdmin");
+        return modelAndView;
+    }
+
+    // TERCERO CARGA LA VISTA PARA LISTAR ASIGNATURAS.
+    @GetMapping("/listarAsignaturasAdmin")
+    public ModelAndView listarAsignaturas() {
+        ModelAndView mv = new ModelAndView();
+        List<Asignaturas> asignaturas = asignaturasRepository.findAll();
+        mv.addObject("asignaturas", asignaturas);
+        mv.setViewName("pages/administrador/subject/readSubjectAdmin");
+        return mv;
+    }
+
+    //-----------------PROFESOR----------------------
 }
+
