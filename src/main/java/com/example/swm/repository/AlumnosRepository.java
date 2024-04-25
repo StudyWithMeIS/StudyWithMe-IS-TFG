deff9180d85940bf9b9ee07fe92f02186eadbac9
SergiooCS <sergio.camino@campusfp.es>
2024-04-25 21:16:13 +0200

    feat(backend): borrar alumnos desde admin

--- src/main/java/com/example/swm/repository/AlumnosRepository.java
+++ src/main/java/com/example/swm/repository/AlumnosRepository.java
@@ -3,6 +3,8 @@ package com.example.swm.repository;
 import com.example.swm.entity.Alumnos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlumnosRepository extends JpaRepository<Alumnos, Integer> {
+import java.util.Optional;
 
+public interface AlumnosRepository extends JpaRepository<Alumnos, Integer> {
+    Optional<Alumnos> findByNif_alumno(String nif_alumno);
 }
