b05962dd16aae70dce902c5f207c373ba57850b3
IslaP-DEV <isla.peinado@campusfp.es>
2024-04-29 18:17:02 +0200

    CONFLICTOS ARREGLADOS

--- src/main/java/com/example/swm/repository/AlumnosRepository.java
+++ src/main/java/com/example/swm/repository/AlumnosRepository.java
@@ -2,7 +2,12 @@ package com.example.swm.repository;
 
 import com.example.swm.entity.Alumnos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AlumnosRepository extends JpaRepository<Alumnos, Integer> {
-
+    @Query("SELECT a FROM Alumnos a WHERE a.nif_alumno = :nif_alumno")
+    Optional<Alumnos> findAlumnosByNif(@Param("nif_alumno") String nif_alumno);
 }
