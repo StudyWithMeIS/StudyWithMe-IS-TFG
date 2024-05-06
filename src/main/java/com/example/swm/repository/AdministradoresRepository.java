f2d0673365503e7d3f4d0106ee7bd503b2aae5e7
SergiooCS <sergio.camino@campusfp.es>
2024-05-06 21:31:56 +0200

    feat(backend): CRUD DE ADMINISTRADORES CREADO

--- src/main/java/com/example/swm/repository/AdministradoresRepository.java
+++ src/main/java/com/example/swm/repository/AdministradoresRepository.java
@@ -2,7 +2,14 @@ package com.example.swm.repository;
 
 
 import com.example.swm.entity.Administradores;
+import com.example.swm.entity.Alumnos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AdministradoresRepository extends JpaRepository<Administradores, Integer> {
+    @Query("SELECT a FROM Administradores a WHERE a.nif_admin = :nif_admin")
+    Optional<Administradores> findAdministradorByNif(@Param("nif_admin") String nif_admin);
 }
