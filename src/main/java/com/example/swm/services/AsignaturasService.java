218500b0e7378a7176d142e42eb6cc3e056a61e5
SergiooCS <sergio.camino@campusfp.es>
2024-05-16 00:39:48 +0200

    feat(backend): CRUD de TAREAS CREADO

--- src/main/java/com/example/swm/services/AsignaturasService.java
+++ src/main/java/com/example/swm/services/AsignaturasService.java
@@ -19,21 +19,21 @@ public class AsignaturasService {
     @Autowired
     private AsignaturasRepository asignaturasRepository;
 
-    @Autowired
-    private CursosAsignaturasRepository cursosAsignaturasRepository;
+//    @Autowired
+//    private CursosAsignaturasRepository cursosAsignaturasRepository;
 
 
-    public void eliminarAsignatura(int id_asignatura) {
-        Asignaturas asignatura = asignaturasRepository.findById(id_asignatura).orElse(null);
-        if (asignatura != null) {
-            List<CursosAsignaturas> cursosAsignaturas = cursosAsignaturasRepository.findCursosASignaturasByAsignaturaId(id_asignatura);
-            for (CursosAsignaturas cursosAsignatura : cursosAsignaturas) {
-                cursosAsignaturasRepository.delete(cursosAsignatura);
-            }
-            asignaturasRepository.delete(asignatura);
-            System.out.println("Asignatura eliminada correctamente.");
-        } else {
-            System.out.println("No se encontró ningúna asignatura con ID: " + id_asignatura);
-        }
-    }
+//    public void eliminarAsignatura(int id_asignatura) {
+//        Asignaturas asignatura = asignaturasRepository.findById(id_asignatura).orElse(null);
+//        if (asignatura != null) {
+//            List<CursosAsignaturas> cursosAsignaturas = cursosAsignaturasRepository.findCursosASignaturasByAsignaturaId(id_asignatura);
+//            for (CursosAsignaturas cursosAsignatura : cursosAsignaturas) {
+//                cursosAsignaturasRepository.delete(cursosAsignatura);
+//            }
+//            asignaturasRepository.delete(asignatura);
+//            System.out.println("Asignatura eliminada correctamente.");
+//        } else {
+//            System.out.println("No se encontró ningúna asignatura con ID: " + id_asignatura);
+//        }
+//    }
 }
